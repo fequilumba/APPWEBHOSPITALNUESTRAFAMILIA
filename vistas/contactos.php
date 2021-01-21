465e58e270946b5951b0cacd89e6dab734d53250
fausto <faustoquilumba@gmail.com>
2021-01-21 13:24:04 -0500

    Vista Contactos
    -diseño con css
    -map responsive

--- vistas/contactos.php
+++ vistas/contactos.php
@@ -6,56 +6,66 @@
         <!-- Main content -->
         <section class="content">
             <div class="row">
-              <div class="col-md-12">
-                  <div class="box">
-                    <div class="box-header with-border">
+                <div class="col-md-12">
+                    <div class="box">
+                    <h1 class="titulo">CONTACTOS</h1>
+                        <div class="box-header with-border">
                          <!--inicio formulario contactos-->
-                            <h1>CONTACTOS</h1>
+                            
                             <br>
-                            <div style="text-align:center;">
-                            <img src="../files/img/LOGO.png" alt="Imagen Logo" height="100px" width="180px">
+                            <div class="img">
+                                <img src="../files/img/LOGO.png" alt="Imagen Logo" height="100px" width="180px">
                             </div>
                             <br>
                             <div>
-                            <div class="columna">
-                                <div style="text-align: center;"><img src="../public/img/phone.png" alt="#"></div>
-                                <h6 style="text-align: center; font-family: cursive;"><b>LLAMENOS</b></h6>
-                                <ul class="list-unstyled text-small">
-                                <P style="text-align: center;">
-                                    <b>Citas:</b> (+593) 979589877<br>
-                                    <b>Gerencia:</b> (+593) 899788598<br></P>
-                                </ul>
-                            </div>
-                            <div class="columna">
-                            <div style="text-align: center;"><img src="../public/img/clock.png" alt="#"></div>
-                                <h6 style="text-align: center; font-family: cursive;"><b>HORARIOS DE ATENCION</b></h6>
-                                <ul class="list-unstyled text-small">
-                                <P style="text-align: center;">
-                                    <b> Lunes a Viernes: </b>09h00 a 12h00 <br>
-                                    <b> Lunes a Viernes: </b>14h00 a 16h00 <br></P>
-                                </ul>
-                            </div>
-                        <div class="columna">
-                            <div style="text-align: center;"><img src="../public/img/direccion.png" alt="#"></div>
-                                <h6 class="color-text" style="text-align: center; font-family: cursive;"><b>DIRECCION</b></h6>
-                                <ul class="list-unstyled text-small">
-                                    <P style="text-align: center;">Malimpia 445, Quito 170131</P>
-                                </ul>
-                            </div>
-                        </div>
-     
-                        <br>
-                        <!--mapa-->
-                        <div style="text-align:center;">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14955.01096747024!2d-78.55059576253925!3d-0.2700112320026364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x233538c4d1964cf7!2sHospitales%20publicos!5e0!3m2!1ses-419!2sec!4v1611204078448!5m2!1ses-419!2sec" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                        </div>
-                         <!--fin formulario contactos-->
-                    </div>
+                                <div class="columna">
+                                    <div style="text-align: center;">
+                                        <img src="../public/img/phone.png" alt="#">
+                                    </div>
+                                    <h6 style="text-align: center; font-family: cursive;"><b>LLAMENOS</b></h6>
+                                    <ul class="list-unstyled text-small">
+                                        <P style="text-align: center;">
+                                            <b>Citas:</b> (+593) 979589877<br>
+                                            <b>Gerencia:</b> (+593) 899788598<br>
+                                        </P>
+                                    </ul>
+                                </div>
+                                
+                                <div class="columna">
+                                    <div style="text-align: center;">
+                                        <img src="../public/img/clock.png" alt="#">
+                                    </div>
+                                    <h6 style="text-align: center; font-family: cursive;"><b>HORARIOS DE ATENCION</b></h6>
+                                    <ul class="list-unstyled text-small">
+                                        <P style="text-align: center;">
+                                            <b> Lunes a Viernes: </b>09h00 a 12h00 <br>
+                                            <b> Lunes a Viernes: </b>14h00 a 16h00 <br>
+                                        </P>
+                                    </ul>    
+                                </div>
 
-                    <!--Fin centro -->
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-          </div><!-- /.row -->
+                                <div class="columna">
+                                    <div style="text-align: center;">
+                                        <img src="../public/img/direccion.png" alt="#">
+                                    </div>
+                                    <h6 style="text-align: center; font-family: cursive;"><b>DIRECCION</b></h6>
+                                    <ul class="list-unstyled text-small">
+                                        <P style="text-align: center;">Malimpia 445, Quito 170131</P>
+                                    </ul>
+                                </div>
+                            </div>  <!--fin formulario contactos-->
+                            <!--Fin centro -->
+                        </div>                                                   
+                        <!--mapa-->
+                        
+                        <div class="google-maps">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14955.01096747024!2d-78.55059576253925!3d-0.2700112320026364!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x233538c4d1964cf7!2sHospitales%20publicos!5e0!3m2!1ses-419!2sec!4v1611204078448!5m2!1ses-419!2sec" frameborder="0"></iframe>
+                        </div>
+                        
+                    </div><!-- /.box -->
+                    
+                </div><!-- /.col -->    
+            </div><!-- /.row -->
       </section><!-- /.content -->
 
     </div><!-- /.content-wrapper -->
