91e9b2362e5c001ccb5793bc330a0b1e1dd0c2a4
jorgeLQ <56508598+jorgeLQ@users.noreply.github.com>
2021-01-31 22:57:39 -0500

    Historial medico
    visualizar historial medico con nombres del paciente, nombres del doctor, especialidad y diagnostico

--- vistas/header.php
+++ vistas/header.php
@@ -232,7 +232,7 @@
                   <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                  <li><a href="#"><i class="fa fa-circle-o"></i> Historial Medico</a></li>
+                  <li><a href="historialmedico.php"><i class="fa fa-circle-o"></i> Historial Medico</a></li>
                   <li><a href="#"><i class="fa fa-circle-o"></i> Recetas</a></li>
                   <li><a href="#"><i class="fa fa-circle-o"></i> Examenes</a></li>                
                 </ul>
