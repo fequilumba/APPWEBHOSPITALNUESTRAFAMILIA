805ba6dc030d85a53ef15d0931b49676ef4f1159
jorgeLQ <56508598+jorgeLQ@users.noreply.github.com>
2021-03-03 09:32:52 -0500

    Arreglo de pantalla responsivs
    Se corrigio el modulo mi perfil y se soluciono los margenes se ajusten a la pantalla responsiva

--- vistas/miperfil.php
+++ vistas/miperfil.php
@@ -11,116 +11,114 @@ if(!isset($_SESSION["nombres"])) //si la validable de sesion no existe.. signifi
   if ($_SESSION['rol_idrol']==1||$_SESSION['rol_idrol']==2||$_SESSION['rol_idrol']==3) {
 ?>
 <!--Contenido-->
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
         
-        <!-- Main content -->
-        <section class="content">
-            <div class="row">
-              <div class="col-md-12">
-                  <div class="box">
-                    <div class="box-header with-border">
-                          <h1 class="box-title">Mi Perfil <!--<button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Nuevo Paciente</button>--></h1>
-                        <div class="box-tools pull-right">
-                        </div>
-                    </div>
-                    <!-- /.box-header -->
-                    <!-- centro -->
-                    <div class="panel-body table-responsive" id="listadoregistros">
-                        <table id="tbllistado" class="table table-striped
-                        table-bordered table-condensed table-hover dt-responsive DT">
-                          <thead>
-                            <th>Opciones</th>
-                            <th>Cédula</th>
-                            <th>Nombres</th>
-                            <th>Email</th>
-                            <th>teléfono</th>
-                            <th>Foto</th>
-                            <th>Estado</th>
-                          </thead>
-                          <tbody>
+  <!-- Main content -->
+  <section class="content">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header with-border">
+            <h1 class="box-title">Mi Perfil <!--<button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Nuevo Paciente</button>--></h1>
+            <div class="box-tools pull-right">
+            </div>
+          </div>
 
-                          </tbody>
-                          <tfoot>
-                            <th>Opciones</th>
-                            <th>Cédula</th>
-                            <th>Nombres</th>
-                            <th>Email</th>
-                            <th>teléfono</th>
-                            <th>Foto</th>
-                            <th>Estado</th>
-                          </tfoot>
-                        </table>
-                    </div>
-                    <div class="panel-body" style="height: 600px;" id="formularioregistros">
-                        <form name="formulario" id="formulario" method="POST">
-                          <div class="form-row">
-                              <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Nombres</label>
-                                <input type="hidden" name="idpersona" id="idpersona">
-                                <input type="hidden" name="cedula" id="cedula" class="form-control" onkeypress="return soloLetras(event)"  maxlength="45" placeholder="Nombres" required>
-                                <input type="text" name="nombres" id="nombres" class="form-control" onkeypress="return soloLetras(event)"  maxlength="45" placeholder="Nombres" required>
-                              </div>
+          <!-- centro -->
+          <div class="panel-body table-responsive" id="listadoregistros">
+            <table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover dt-responsive DT">
+              <thead>
+                <th>Opciones</th>
+                <th>Cédula</th>
+                <th>Nombres</th>
+                <th>Email</th>
+                <th>teléfono</th>
+                <th>Foto</th>
+                <th>Estado</th>
+              </thead>
+              <tbody>
 
-                              <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Apellidos</label>
-                                <input type="text" name="apellidos" id="apellidos" class="form-control" onkeypress="return soloLetras(event)"  maxlength="45" placeholder="Apellidos"required>
-                              </div>
+              </tbody>
+              <tfoot>
+                <th>Opciones</th>
+                <th>Cédula</th>
+                <th>Nombres</th>
+                <th>Email</th>
+                <th>teléfono</th>
+                <th>Foto</th>
+                <th>Estado</th>
+              </tfoot>
+            </table>
+          </div> <!-- panel-body datetable -->
 
-                              <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Email</label>
-                                <input type="text" name="email" id="email" maxlength="45" class="form-control"  placeholder="email@address.com" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" required>
-                              </div>
+          <div class="panel-body" id="formularioregistros">
+            <form name="formulario" id="formulario" method="POST">
+              
+              <div class="form-row">
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Nombres</label>
+                  <input type="hidden" name="idpersona" id="idpersona">
+                  <input type="hidden" name="cedula" id="cedula" class="form-control" onkeypress="return soloLetras(event)"  maxlength="45" placeholder="Nombres" required>
+                  <input type="text" name="nombres" id="nombres" class="form-control" onkeypress="return soloLetras(event)"  maxlength="45" placeholder="Nombres" required>
+                </div>
 
-                              <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Teléfono</label>
-                                <input type="text" name="telefono" id="telefono" maxlength="10" minlength="10" class="form-control" onkeypress="return soloNumeros(event)" placeholder="Teléfono" required>
-                              </div>
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Apellidos</label>
+                  <input type="text" name="apellidos" id="apellidos" class="form-control" onkeypress="return soloLetras(event)"  maxlength="45" placeholder="Apellidos"required>
+                </div>
 
-                              <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Dirección</label>
-                                <input type="text" name="direccion" id="direccion" class="form-control"  maxlength="45" placeholder="Dirección" required>
-                              </div>
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Email</label>
+                  <input type="text" name="email" id="email" maxlength="45" class="form-control"  placeholder="email@address.com" pattern="[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}" required>
+                </div>
 
-                              <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Ciudad</label>
-                                <input type="text" name="ciudad_residencia" id="ciudad_residencia" class="form-control" onkeypress="return soloLetras(event)" maxlength="45" placeholder="Ciudad" required>
-                              </div>
-                          
-                          
-                            <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
-                            <label>Imagen:</label> 
-                            <div class="file-select inner-addon left-addon" id="src-file1" >
-                            <i class="glyphicon fas fa-camera-retro"></i>
-                            <input type="file" class="" name="imagen" id="imagen" accept="image/x-png,image/gif,image/jpeg">
-                            </div>
-                            <br>
-                            <input type="hidden" name="imagenactual" id="imagenactual">
-                            <img src="" width="150px" height="120px" id="imagenmuestra">
-                            </div>
-                            </div><!-- /.form-row -->
-                          <div class="form-row">
-                            <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Contraseña</label>
-                                <input type="password" name="contrasenia" id="contrasenia" class="form-control"  minlength="8" placeholder="********">
-                              </div>
-                              
-                          </div><!-- /.form-row -->
-                          <div class="form-row">
-                          <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <label for="">Confirmar Contraseña</label>
-                                <input type="password" name="confircontrasenia" id="confircontrasenia" class="form-control" minlength="8" placeholder="********">
-                              </div>
-                              <div id="error2">
-                              </div>
-                              <div id="text">
-                              </div>
-                          </div><!-- /.form-row -->
-                          <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <button class="btn btn-primary" type="submit" 
-                            id="btnGuardar"><i class="fa fa-save"> Guardar</i></button>
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Teléfono</label>
+                  <input type="text" name="telefono" id="telefono" maxlength="10" minlength="10" class="form-control" onkeypress="return soloNumeros(event)" placeholder="Teléfono" required>
+                </div>
 
-                            <button class="btn btn-danger" onclick="cancelarform()"
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Dirección</label>
+                  <input type="text" name="direccion" id="direccion" class="form-control"  maxlength="45" placeholder="Dirección" required>
+                </div>
+
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Ciudad</label>
+                  <input type="text" name="ciudad_residencia" id="ciudad_residencia" class="form-control" onkeypress="return soloLetras(event)" maxlength="45" placeholder="Ciudad" required>
+                </div>
+                                          
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
+                  <label>Imagen:</label> 
+                  <div class="file-select inner-addon left-addon" id="src-file1" >
+                    <i class="glyphicon fas fa-camera-retro"></i>
+                    <input type="file" class="" name="imagen" id="imagen" accept="image/x-png,image/gif,image/jpeg">
+                  </div>
+                  <br>
+                  <input type="hidden" name="imagenactual" id="imagenactual">
+                  <img src="" width="150px" height="120px" id="imagenmuestra">
+                </div>
+              </div><!-- /.form-row -->
+              
+              <div class="form-row">
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Contraseña</label>
+                  <input type="password" name="contrasenia" id="contrasenia" class="form-control"  minlength="8" placeholder="********">
+                </div>
+                                            
+                <div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                  <label for="">Confirmar Contraseña</label>
+                  <input type="password" name="confircontrasenia" id="confircontrasenia" class="form-control" minlength="8" placeholder="********">
+                </div>
+
+                <div id="error2"></div>
+                <div id="text"></div>
+              </div><!-- /.form-row -->
+
+              <div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <button class="btn btn-primary" type="submit" id="btnGuardar"><i class="fa fa-save"> Guardar</i></button>
+
+                <button class="btn btn-danger" onclick="cancelarform()"
                             type="button"><i class="fa fa-arrow-circle-left"> Cancelar</i></button>
 
                           </div>
