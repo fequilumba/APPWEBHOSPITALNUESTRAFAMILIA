5606aec8ceeba962a7be4e2f9ca0dab2637ea5e1
fausto <faustoquilumba@gmail.com>
2021-04-08 19:22:22 -0500

    Agregando propiedades
    Se agrego la propiedad table-responsive para que la tabla se ajuste al tamaño de un celular.

--- vistas/miperfil.php
+++ vistas/miperfil.php
@@ -30,7 +30,7 @@ if(!isset($_SESSION["nombres"])) { //si la validable de sesion no existe.. signi
         <div class="col-12">
           <div class="card">
             <div class="card-body">
-              <div class="panel-body" id="listadoregistros">
+              <div class="panel-body table-responsive" id="listadoregistros">
                 <table id="tbllistado" class="table table-striped table-bordered table-hover dt-responsive DT nowrap" style="width:100%">
                   <thead>
                     <th>Opciones</th>
