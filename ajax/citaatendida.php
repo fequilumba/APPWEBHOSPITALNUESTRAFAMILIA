8f36676e75b1d1bbbf789044e1162127a52d6c26
jorgeLQ <56508598+jorgeLQ@users.noreply.github.com>
2021-04-10 12:26:58 -0500

    receta y examen
    -restriccion de acceso para visualizar las recetas y examenes en formato PDF

--- ajax/citaatendida.php
+++ ajax/citaatendida.php
@@ -9,26 +9,6 @@
     
     
     switch ($_GET["op"]) {
-        /*case 'guardaryeditar':
-                if (empty($idtipo_examen)) {
-                    $rspta=$examenmedico->insertar($seleccita,$nombre);
-                    echo $rspta ? "Exámen registrado" : "No se pudo registrar los exámenes";
-                    
-                }else{
-                    $rspta=$examenmedico->editar($idtipo_examen, $nombre);
-                    echo $rspta ? "Exámen actualizado" : "No se pudo actualizar los exámenes";                
-                }
-            break;*/
-        /*case 'desactivar':
-            $rspta=$especialidad->desactivar($idespecialidad);
-            echo $rspta ? "Especialidad desactivada" : "No se pudo desactivar la especialidad";
-
-            break;
-        case 'activar':
-            $rspta=$especialidad->activar($idespecialidad);
-            echo $rspta ? "Especialidad activada" : "No se pudo activar la especialidad";
-
-            break;*/
         case 'mostrar':
             $rspta=$citaatendida->mostrar($idcita_medica);
             echo json_encode($rspta);
@@ -38,13 +18,12 @@
             $data = Array();
             while ($reg=$rspta->fetch_object()) {
                 $data[]= array(
-                    "0"=>$reg->idcita_medica,
-                    "1"=>$reg->especialidad,
-                    "2"=>$reg->nombre,
-                    "3"=>$reg->telefono,
-                    "4"=>$reg->fecha_cita,
-                    "5"=>$reg->hora_cita,
-                    "6"=>$reg->estado
+                    "0"=>$reg->especialidad,
+                    "1"=>$reg->nombre,
+                    "2"=>$reg->telefono,
+                    "3"=>$reg->fecha_cita,
+                    "4"=>$reg->hora_cita,
+                    "5"=>$reg->estado
                 );
             }
             $results = array(
@@ -54,24 +33,6 @@
                 "aaData"=>$data);    
                 echo json_encode($results);   
             break;
-        /*case 'imagen':
-            require_once "../modelos/Examen.php";
-            $examen = new Examen();
-            $rspta=$examen->listarExamenImagen();
-
-            while ($reg = $rspta->fetch_object()) {
-                echo '<li> <input type="checkbox" name "imagen[]" value="'.$reg->idtipo_examen.'">'.$reg->nombre.'</li>';
-            }
-            break;
-        case 'sangre':
-            require_once "../modelos/Examen.php";
-            $examen = new Examen();
-            $rspta=$examen->listarExamenSangre();
-    
-            while ($reg = $rspta->fetch_object()) {
-                echo '<li> <input type="checkbox" name "sangre[]" value="'.$reg->idtipo_examen.'">'.$reg->nombre.'</li>';
-            }
-            break;*/
     }
 
 ?>
